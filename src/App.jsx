7d3b213d972025d40c05a4b8dd378fdb5b634732
hasanposh <hmasif147@gmail.com>
2024-03-15 19:29:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h3 className="text-3xl font-bold underline">Discover an exceptional cooking class tailored for you!</h3>
+    </>
+  )
+}
+
+export default App
